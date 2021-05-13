2d062d4a65cb995f819ef34998db4a33c973cdf3
Jaslan <Jaslan.dlnv@gmail.com>
2021-05-13 20:24:11 +0500

    componentDidUpdate

--- src/components/Main/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Main/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -26,7 +26,16 @@ class ProfileStatus extends React.Component {
       });
    }
 
+   componentDidUpdate(prevProps, prevState) {
+      if(prevProps.status !== this.props.status) {
+         this.setState({
+            status: this.props.status,
+         });
+      }
+   }
+
    render() {
+      console.log('render')
       return (
          <div className={s.status_wrapper}>
             { !this.state.editMode && 
