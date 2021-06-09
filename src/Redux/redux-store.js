e9763b239f82cf8bd1030e337298628e3cee4da8
Jaslan <Jaslan.dlnv@gmail.com>
2021-06-09 19:49:22 +0500

    redux devtools extention compose

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore } from "redux";
+import {applyMiddleware, combineReducers, compose, createStore } from "redux";
 import dialogReducer from "./dialog-reducer";
 import profileReducer from "./profile-reducer";
 import sideBarReducer from "./sideBar-reducer";
@@ -17,8 +17,10 @@ let reducers = combineReducers({
    app: appReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
-window.store = store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleWare)));
+
+window.__store__ = store;
 
 
 export default store;
