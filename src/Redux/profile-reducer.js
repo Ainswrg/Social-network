b93876834f7515a4cb7ad6d128b6a790b58a7d58
Jaslan <Jaslan.dlnv@gmail.com>
2021-06-09 15:30:41 +0500

    Tests: tdd, jest,  add profile-reducers.test.js.

--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -3,6 +3,7 @@ import { profileAPI } from "../api/api";
 const ADD_POST = "ADD-POST";
 const SET_USER_PROFILE = "SET_USER_PROFILE";
 const SET_STATUS = "SET_STATUS";
+const DELETE_POST = "DELETE_POST";
 
 let initialState = {
    posts: [
@@ -39,16 +40,21 @@ const profileReducer = (state = initialState, action) => {
             status: action.status,
          };
       }
+      case DELETE_POST: {
+         return {
+            ...state, posts: state.posts.filter(p => p.id !== action.postId)
+         }
+      }
       default:
          return state;
    }
 };
 
-export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText})
-
-export const setStatus = (status) => ({ type: SET_STATUS, status });
+export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText});
 export const setUserProfile = (profile) => ({ 
    type: SET_USER_PROFILE, profile,});
+export const setStatus = (status) => ({ type: SET_STATUS, status });
+export const deletePost = (postId) => ({type: DELETE_POST, postId})
 
 
 export const getStatus = (userId) => (dispatch) => {
