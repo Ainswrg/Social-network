9a08a3945e71200660c39b3b26c38891fc3e7d6d
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-04 19:45:14 +0500

    refactor: migration to TS: added types for HOC's

---
+++ src/hoc/withSuspense.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Preloader from "../components/common/Preloader/Preloader";
+
+
+
+export function withSuspense<WCP> (WrappedComponent: React.ComponentType<WCP>) {
+   return (props: WCP) => {
+       return <React.Suspense fallback={<Preloader />} >
+           <WrappedComponent {...props} />
+       </React.Suspense>
+   };
+}
