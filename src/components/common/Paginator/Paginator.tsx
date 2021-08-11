bdfd2ff8ddad28869533089b530502e680e388e1
Jaslan <Jaslan.dlnv@gmail.com>
2021-08-11 19:11:10 +0500

    refactor: added test for Pagination

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -2,72 +2,74 @@ import React, { useState } from "react";
 import { StyledPageNumber, StyledPaginator } from "./Styles";
 
 type PropsType = {
-   totalItemsCount: number
-   pageSize: number
-   currentPage?: number
-   portionSize?: number
-   onPageChanged?: (pageNumber: number) => void
-}
+  totalItemsCount: number;
+  pageSize: number;
+  portionSize?: number;
+  currentPage?: number;
+  onPageChanged?: (pageNumber: number) => void;
+};
 
 let Paginator: React.FC<PropsType> = ({
-   totalItemsCount,
-   pageSize,
-   currentPage,
-   onPageChanged = x => x,
-   portionSize = 10,
+  totalItemsCount,
+  pageSize,
+  currentPage,
+  onPageChanged = (x) => x,
+  portionSize = 10,
 }) => {
-   let pagesCount = Math.ceil(totalItemsCount / pageSize);
+  let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
-   let pages: Array<number> = [];
-   for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-   }
+  let pages: Array<number> = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
 
-   let portionCount = Math.ceil(pagesCount / portionSize);
-   let [portionNumber, setPortionNumber] = useState(1);
-   let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
-   let rightPortionPageNumber = portionNumber * portionSize;
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  let [portionNumber, setPortionNumber] = useState(1);
 
-   return (
-      <StyledPaginator>
-         {portionNumber > 1 && (
-            <button
-               onClick={() => {
-                  setPortionNumber(portionNumber - 1);
-               }}
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionPageNumber = portionNumber * portionSize;
+
+  return (
+    <StyledPaginator>
+      {portionNumber > 1 && (
+        <button
+          onClick={() => {
+            setPortionNumber(portionNumber - 1);
+          }}
+        >
+          <i className='fa fa-chevron-circle-left'></i>
+        </button>
+      )}
+
+      {pages
+        .filter(
+          (p) => p >= leftPortionPageNumber && p <= rightPortionPageNumber
+        )
+        .map((p) => {
+          return (
+            <StyledPageNumber
+              key={p}
+              onClick={() => {
+                onPageChanged(p);
+              }}
+              active={currentPage === p}
+              primary
             >
-               <i className="fa fa-chevron-circle-left"></i>
-            </button>
-         )}
-
-         {pages
-            .filter(
-               (p) => p >= leftPortionPageNumber && p <= rightPortionPageNumber
-            )
-            .map((p) => {
-               return (
-                  <StyledPageNumber
-                     key={p}
-                     onClick={() => {
-                        onPageChanged(p);
-                     }}
-                     active={currentPage === p}
-                  >
-                     {p}
-                  </StyledPageNumber>
-               );
-            })}
-         {portionCount > portionNumber && (
-            <button
-               onClick={() => {
-                  setPortionNumber(portionNumber + 1);
-               }}
-            >
-               <i className="fa fa-chevron-circle-right"></i>
-            </button>
-         )}
-      </StyledPaginator>
-   );
+              {p}
+            </StyledPageNumber>
+          );
+        })}
+      {portionCount > portionNumber && (
+        <button
+          onClick={() => {
+            setPortionNumber(portionNumber + 1);
+          }}
+        >
+          <i className='fa fa-chevron-circle-right'></i>
+        </button>
+      )}
+    </StyledPaginator>
+  );
 };
 
 export default Paginator;
